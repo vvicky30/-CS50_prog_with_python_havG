b2f1045d083816349ece863451b7511fd1e7a97d
Vedant Bhatt <vickyvedant365@gmail.com>
2024-03-07 19:05:00 +0530

    defining fns/lambda fns usage links from Geeks for geeks.

---
+++ defining_function.py
@@ -0,0 +1,12 @@
+#here first we are going to first defining main-fn wehre all driver code is going to exist for initial and final excecution , 
+#where we're going to call several fns defined out of the main-fn by passing relevent arguments,parameters/var
+#we're going to define our own sets or chains of fns out of the main-fn all with their operational definitions and with their functional parameters (optional parameters, necesaary parameters with default values etc), and then going to return the final furnished o/p from respective fns to corresponding functional call in main-fns
+
+
+'''
+https://www.geeksforgeeks.org/python-functions/
+
+https://www.geeksforgeeks.org/python-lambda-anonymous-functions-filter-map-reduce/
+
+https://www.geeksforgeeks.org/python-program-to-find-second-largest-number-in-a-list/
+'''
