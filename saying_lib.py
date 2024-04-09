e40cd354831773fd3e8e075643e5269c855a223d
Vedant Bhatt <vickyvedant365@gmail.com>
2024-04-09 19:43:22 +0530

    saying liberay/module-2

--- saying_lib.py
+++ saying_lib.py
@@ -1,2 +1,4 @@
 # going to make our own liberary/module of python that will be used by us here locally by importing into another project 
 # or program or we can simply upload it on cloud-server so that it can acess by contributor on that server or on that same infra-level
+#or we can contribute to open-source liberary-hub or repository or like PYPI web-site for open source contributors for python. 
+   # so that from there various users across the globe can install our liberary through pip or by other ways and use it for repective purposes.
