3f315900bb87b9dc5e6d5308bdaccb9b0732af34
Vedant Bhatt <vickyvedant365@gmail.com>
2024-04-15 21:37:45 +0530

    Here this py-script i created for experimenting regarding snake-say module authored by me , gona be making module by packaging the project as python module, which i m gona acess through-out my python in system even ouside that local folder of that particular project.

---
+++ sanake_say_exp.py
@@ -0,0 +1,2 @@
+# here this py script i created for experimenting regarding snake-say module authored by me , gona be making module by packaging the project as python module 
+# which i m gona acess through-out my python in system even ouside that local folder of that particular project.
