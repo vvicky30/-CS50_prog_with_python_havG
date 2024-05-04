eeb46f530029a7444a53fda2f54d724dc6a108db
Vedant Bhatt <vickyvedant365@gmail.com>
2024-05-04 16:07:40 +0530

    As we alll know that __init__.py file used for initialization of source code of python package , This file made for the purpose so that python interpreter or any python framework recognise the folder as a python package Here this file we made so that hello_test_files folder will be recognised as package by pytest framework As this folder consiting of one(test_hello.py) or might be multiple(in future) test_scripts file for testing several programs and module's source code.

--- hello_test_files/__init__.py
+++ hello_test_files/__init__.py
@@ -2,4 +2,3 @@
 # this file made for the purpose so that python interpreter or any python framework recognise the folder as a python package 
 # here this file we made so that hello_test_files folder will be recognised as package by pytest framework 
 # as this folder consiting of one(test_hello.py) or might be multiple(in future) test_scripts file for testing several programs and module's source code.
-
