994e0d0cdb776c7030cdf7a9cc4fce0f8985fb71
Vedant Bhatt <vickyvedant365@gmail.com>
2024-05-20 18:19:00 +0530

    Illustration of usage of type-hints in the code and the significance of 'mypy'  type-tester which;s trsting over type-hinted codes,

--- Meow.py
+++ Meow.py
@@ -39,3 +39,49 @@ Found 1 error in 1 file (checked 1 source file)
 //and tell us expression to be assigned is of type str whereas variable has type int(as we hinted it with int-class)
   
 '''
+'''
+  
+# this time we're going to take int value from user
+def meow(n:int)-> str:   # n should be the int , so we hinted it as of int class here like this 'n:int'
+       # here we also specify one more type-hint that this fn should return value of str-type with the help of this operator : -> str (after function arguments) 
+    for _ in range(n):
+        print("Meow!")  # this function just printing meow as byproduct 
+                           # not returning 'meow' of str clss/type 
+
+number: int = int(input("what's number: "))  # and here we hinted number to be of int-class, meaning we  want number variable to be assign value of int-class only
+
+m:str = meow(number)   # as function should ideally return 'meow' of str-type, this variable should be of type str only
+print(m)  
+
+#now we tried to run a above code with mypy :
+mypy Meow.py
+o/p>Meow.py:43: error: Missing return statement  [return]
+Meow.py:49: error: Incompatible types in assignment (expression has type "str", variable has type "int")  [assignment]
+Found 2 errors in 1 file (checked 1 source file) 
+
+//this it found two errors one at line 43 and other at 49 line 
+// first error is saying missing return as fn doesn't retuning any value just forget about returning str-type value
+// second error is that it's not assigning anything meaning 'None' to the variable m here
+  
+'''
+
+'''
+
+# this times we resolve above type errors , this time fn will return hinted str type value 
+# and m- variable will be assigned with str-type string 
+def meow(n:int)-> str:   # n should be the int , so we hinted it as of int class here like this 'n:int'
+       # here we also specify one more type-hint that this fn should return value of str-type with the help of this operator : -> str (after function arguments) 
+    return "Meow! \n" * n  
+
+number: int = int(input("what's number: "))  # and here we hinted number to be of int-class, meaning we  want number variable to be assign value of int-class only
+
+m:str = meow(number)   # as function should ideally return 'meow' of str-type, this variable should be of type str only
+print(m, end="")  
+
+#now we tried to run a above code with mypy :
+mypy Meow.py
+o/p> Success: no issues found in 1 source file    
+// no isseues related to type-error found in this code now 
+// as all variables and functions with hinted repective d-type satisfied while assigning values and retruning values
+
+'''
