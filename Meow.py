820926d355bdcf3b9f9d373d9a66b9305c45cbeb
Vedant Bhatt <vickyvedant365@gmail.com>
2024-05-20 17:51:00 +0530

    Illustration of usage of type-hints in the code and the significance of 'mypy'  type-tester which;s trsting over type-hinted codes,

--- Meow.py
+++ Meow.py
@@ -16,4 +16,26 @@ meow(number)
 //as this will give me error that :  
 TypeError: 'str' object cannot be interpreted as an integer
 '''
-# there's  
+# there's type-tester promineient in python with this 'mypy' type tester 
+# we can test our codes whether our code have probably any type error or not (quite useful in complex code structure)
+# firstly we have to install it through pip like this : pip install mypy
+# once we install my py we will write our code in hinted method wherever we think it might generate type error like this :
+
+'''
+
+
+def meow(n:int):   # n should be the int , so we hinted it as of int class here like this 'n:int' 
+    for _ in range(n):
+        print("Meow!")
+
+number: int = input("what's number: ")  # and here we hinsted number to be of int-class, meaning we  want number variable to be assign value of int-class only
+meow(number)  
+
+#now we tried to run a above code with mypy :
+mypy Meow.py
+o/p> Meow.py:30: error: Incompatible types in assignment (expression has type "str", variable has type "int")  [assignment]
+Found 1 error in 1 file (checked 1 source file)
+// it shows a possible error at line number 30 where assignment takes place for variable 'number'
+//and tell us expression to be assigned is of type str whereas variable has type int(as we hinted it with int-class)
+  
+'''
