b4aa050b0010a1773d3c5a777e4d975ba523d300
Vedant Bhatt <vickyvedant365@gmail.com>
2024-05-21 15:22:44 +0530

    lets make cli-program to display meow (times of meow user enter) first by conventional method and then by argparser.

--- meow_cli.py
+++ meow_cli.py
@@ -1,5 +1,6 @@
 #let make cli-program to display meow (times of meow user enter) first by conventional method and then by argparser
-
+ 
+'''
 import sys
 
 if len(sys.argv) == 1:  # in case where user doesn't enter anything after file name
@@ -10,5 +11,42 @@ elif len(sys.argv) == 3 and sys.argv[1]=="-n":
 else:   # if user entered anyhing else kinda default case 
     print("Usage of Meow_cli.py: please enter in this format(ex:) \n python Meow_cli.py -n 4")
     
-    
+   
+o/p[on cosole]:
+-python meow_cli.py
+>Meow
+
+-python meow_cli.py -n 6
+>
+ Meow!
+ Meow!
+ Meow!
+ Meow!
+ Meow!
+ Meow!
+
+-python meow_cli.py 5   
+>Usage of Meow_cli.py: please enter in this format(ex:) 
+ python Meow_cli.py -n 4
  
+'''
+
+#now we;re going to use argparaser for making this cli program more systematic
+import argparse  # we don't need sys to import argv
+
+# going to call constructor of argparaser
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-n")  #now going to add arguments or command line options with the help of add_argument -fn  
+
+args = parser.parse_args() # now we're acessing parse.args fn with the help of object parser here 
+                            # and saving its values to the args varibale
+for _ in range(int(args.n)):
+                               # rnage equals to value of n enterted by user while using -n as option; here we convert it first into int.
+                        print("Meow!")       
+
+'''
+o/p[on cosole]:
+
+'''
+                         
