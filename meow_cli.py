4157ceac74033d15930586fa75ddcb9b5feb5057
Vedant Bhatt <vickyvedant365@gmail.com>
2024-05-21 15:22:51 +0530

    lets make cli-program to display meow (times of meow user enter) first by conventional method and then by argparser.

--- meow_cli.py
+++ meow_cli.py
@@ -31,6 +31,8 @@ o/p[on cosole]:
  
 '''
 
+'''
+
 #now we;re going to use argparaser for making this cli program more systematic
 import argparse  # we don't need sys to import argv
 
@@ -45,8 +47,32 @@ for _ in range(int(args.n)):
                                # rnage equals to value of n enterted by user while using -n as option; here we convert it first into int.
                         print("Meow!")       
 
-'''
 o/p[on cosole]:
+-python meow_cli.py -n 4
+Meow!
+Meow!
+Meow!
+Meow!
 
+[if we enter numbers with unexpected option like we here does]:
+-python meow_cli.py -b 4 
+usage: meow_cli.py [-h] [-n N]    //it directs us to use help option which  -h to know what kinds of options available 
+meow_cli.py: error: unrecognized arguments: -b 4   //error of unregnized arguments
+
+[let's have a help option]:
+-meow_cli.py -h  
+
+>usage: meow_cli.py [-h] [-n N]   //it shows that we can use these option with it one is for help [-h] and another option is [-n N] here N denotes that it should be number after -n
+options:
+  -h, --help  show this help message and exit
+  -n N       //as here nothing written to gives user a direction to use this option ....we can write description for specific option like help(above bydefault givng us description)  
 '''
-                         
+
+import argparse
+
+parser =argparse.ArgumentParser()
+parser.add_argument("-n")
+arg = parser.parse_args()
+
+for _ in range(arg.n):
+    print("Meow!")                          
