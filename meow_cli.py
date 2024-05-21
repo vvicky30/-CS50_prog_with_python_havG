9724eb88ae83756d3ec35595e182d27d2390333b
Vedant Bhatt <vickyvedant365@gmail.com>
2024-05-21 15:40:19 +0530

    lets make cli-program to display meow (times of meow user enter) first by conventional method and then by argparser.

--- meow_cli.py
+++ meow_cli.py
@@ -68,11 +68,62 @@ options:
   -n N       //as here nothing written to gives user a direction to use this option ....we can write description for specific option like help(above bydefault givng us description)  
 '''
 
+
+'''
+
 import argparse
 
-parser =argparse.ArgumentParser()
-parser.add_argument("-n")
+parser =argparse.ArgumentParser(description= "it will Moew like a CAT")   #here add description to argument parser 
+parser.add_argument("-n",help="by choosing this option user can enter how many times to Meow!")  #here we add directions to use this argument 
+arg = parser.parse_args()
+
+for _ in range(int(arg.n)):
+    print("Meow!")        
+
+o/p[on cosole]:
+-python meow_cli.py -h  
+>usage: meow_cli.py [-h] [-n N]
+
+it will Moew like a CAT     //now it's giving us the program description
+
+options:
+  -h, --help  show this help message and exit
+  -n N        by choosing this option user can enter how many times  
+              to Meow!
+//and also it's giving us direction to use -n option here
+
+[if we tries to run it like with unexprected arguments or not using any options like this ]:
+-python meow_cli.py  //not using any option
+ for _ in range(int(arg.n)):
+                   ^^^^^^^^^^
+TypeError: int() argument must be a string, a bytes-like object or a real number, not 'NoneType'
+// gives us error
+
+[or if] :
+-python meow_cli.py -n  
+usage: meow_cli.py [-h] [-n N]
+meow_cli.py: error: argument -n: expected one argument    //direct us to be enter one argument after using -n option (it  should be number as specifeid[-n N])
+'''    
+import argparse
+
+parser =argparse.ArgumentParser(description= "it will Moew like a CAT")   #here add description to argument parser 
+parser.add_argument("-n", default= 1, help="by choosing this option user can enter how many times to Meow!", type=int)  #here we add directions to use this argument 
+                                #here i specified default value for option (-n) if user not specifically write any number after choosing this option
+                                   # means by default it will print one 'Meow'
+                                   # also specifed here that values user enter after choosing this option should be int type //so need to convert arg.n in to int during for-loop
 arg = parser.parse_args()
 
 for _ in range(arg.n):
-    print("Meow!")                          
+    print("Meow!")    
+
+'''
+o/p[on cosole]:
+-python meow_cli.py   
+Meow!
+
+[if user enter string instead of number after choosing -n option]:
+python meow_cli.py -n fish
+usage: meow_cli.py [-h] [-n N]
+meow_cli.py: error: argument -n: invalid int value: 'fish'
+//its throws error systematically that fish is invalid int ; which's true
+'''
